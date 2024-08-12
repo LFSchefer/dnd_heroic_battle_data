3cfab29f3c65aaac10031dd8472713d4c0028921
louis frederic schefer <lf.schefer@gmail.com>
2024-08-12 18:41:23 +0200

    create view

--- Scripts/schema.ddl.sql
+++ Scripts/schema.ddl.sql
@@ -176,7 +176,7 @@ CREATE TABLE monsters (
 	intelligence SMALLINT NOT NULL,
 	wisdom SMALLINT NOT NULL,
 	charisma SMALLINT NOT NULL,
---	languages_id bigint NOT NULL,
+--	languages_id bigint,
 	challenge_rating NUMERIC(5,2) NOT NULL,
 	xp integer NOT NULL,
 	image_url varchar(200),
@@ -307,3 +307,18 @@ CREATE TABLE battle_conditions (
 	CONSTRAINT battle_monster_fkey FOREIGN KEY (battle_monster_id) REFERENCES battle_monsters(battle_monster_id) ON DELETE CASCADE,
 	CONSTRAINT condition_id_fkey FOREIGN KEY (condition_id) REFERENCES conditions(condition_id)
 );
+
+CREATE OR REPLACE VIEW monsters_stats AS
+	SELECT  m.monster_id, m.monster_name, m.hit_points, m.hit_dices, m.hit_points_roll, m.strength,
+	m.dexterity, m.constitution, m.intelligence, m.wisdom, m.charisma, m.challenge_rating, m.xp,
+	m.image_url, m.dnd5_url, m.dnd5_native, a.alignment_id , a.alignments_name ,a.description ,
+	s.size_id , s.size_name , s2.sense_id , s2.darkvision , s2.passive_perception ,
+	s3.speed_id , s3.walk , s3.swim , s3.fly , ac.armor_classe_id , ac.armor_type , ac.armor_value 
+	FROM monsters m 
+	LEFT JOIN alignments a ON m.alignment_id = a.alignment_id 
+	INNER JOIN monster_types mt ON m.monster_type_id = mt.monster_type_id
+	INNER JOIN sizes s ON s.size_id = m.size_id
+	INNER JOIN senses s2 ON s2.sense_id = m.sense_id
+	INNER JOIN speeds s3 ON s3.speed_id = m.speed_id
+	INNER JOIN armor_classes ac ON ac.armor_classe_id = m.armor_id
+;
